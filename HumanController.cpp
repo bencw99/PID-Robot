5d182984a52dae5fb7fc168296380eb5dda847e0
Eric Liu <ericxliu@gmail.com>
2014-11-30 16:33:46 -0800

    defined methods n stuff

--- HumanController.cpp
+++ HumanController.cpp
@@ -1,1 +1,26 @@
-hihihi
+#include <WPILib.h>
+#include "HumanController.h"
+#include "DriveTrain.h"
+
+double HumanController::getRotation()
+{
+	return rotationController.GetX();
+}
+
+double HumanController::getMovement()
+{
+	return movementController.GetY();
+}
+
+void HumanController::update()
+{
+	robot->getDriveTrain().setMovement(this->getMovement());
+	robot->getDriveTrain().setRotation(this->getRotation());
+}
+
+HumanController::HumanController(Robot *robot):
+	movementController((uint32_t) PORT_SPEED_CONTROLLER),
+	rotationController((uint32_t) PORT_TURN_CONTROLLER)
+{
+	this->robot = robot;
+}
