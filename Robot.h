fa95597d7b57252ed14cfbec38f83cba8db1b053
bencw99 <ben.cohenwang@gmail.com>
2014-11-30 16:33:20 -0800

    Update Robot.h

--- Robot.h
+++ Robot.h
@@ -1,9 +1,20 @@
 #ifndef ROBOT_H
 #define ROBOT_H
+#include <WPILib.h>
+#include "Constants.h"
+#include "DriveTrain.h"
 
 class Robot
 {
-	
+	private:
+		DriveTrain driveTrain;
+		
+	public:
+		Robot();
+		void update();
+		void disable();
+		
+		DriveTrain getDriveTrain();
 };
 
 #endif
