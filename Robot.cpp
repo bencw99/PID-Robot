897d67642591b7f21ba2636f7a2344d28b8b77a3
bencw99 <ben.cohenwang@gmail.com>
2014-11-30 16:35:05 -0800

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,1 +1,22 @@
 #include "Robot.h"
+
+Robot::Robot():
+	driveTrain()
+{
+	
+}
+
+void Robot::update()
+{
+	driveTrain.update();
+}
+
+void Robot::disable()
+{
+	driveTrain.disable();
+}
+
+DriveTrain Robot::getDriveTrain()
+{
+	return driveTrain;
+}
